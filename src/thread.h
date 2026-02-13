53a0ddabb28af859a09465151917f26922544d8a
87 <dev@87flowers.com>
2026-02-13 18:49:35 +0100

    Bench: 5607071

--- src/thread.h
+++ src/thread.h
@@ -157,10 +157,10 @@ namespace stoat {
         }
 
         [[nodiscard]] inline std::array<Move, 3> sequence(i32 ply) const {
-            if (ply < 3) {
+            if (ply < 5) {
                 return {};
             }
-            return {stack[ply - 3].move, stack[ply - 2].move, stack[ply - 1].move};
+            return {stack[ply - 5].move, stack[ply - 3].move, stack[ply - 1].move};
         }
     };
 } // namespace stoat
