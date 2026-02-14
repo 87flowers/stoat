857d780d7e3f62ae7bacd7bb9502d8b38e348d2f
87 <dev@87flowers.com>
2026-02-14 02:24:49 +0100

    Bench: 5494214

--- src/search.cpp
+++ src/search.cpp
@@ -866,6 +866,10 @@ namespace stoat {
                              .popcount();
                 }
 
+                if (isUnlikelyMove(pos, move)) {
+                    r += 2;
+                }
+
                 const auto reduced = std::min(std::max(newDepth - r, 1), newDepth - 1) + kPvNode;
                 curr.reduction = newDepth - reduced;
                 score = -search(thread, newPos, curr.pv, reduced, ply + 1, -alpha - 1, -alpha, true);
